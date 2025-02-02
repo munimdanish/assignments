f7cc7d1f9dc0ba295630d37d11a9f301758954e0
munimdanish <munimdanish@gmail.com>
2025-02-02 22:49:57 +0500

    Add files via upload

---
+++ assignments/class-2.js
@@ -0,0 +1,20 @@
+let a = 10;
+let b = 15;
+//--------------
+let sum = a + b;
+console.log(sum);                     
+//--------------
+let diff = a - b;
+console.log(diff)
+//--------------
+let prod = a * b;
+console.log(prod);
+//--------------
+let div = a / b ;
+console.log(div);
+//---------------
+let mod = a % b ; 
+console.log(mod);
+
+
+                
