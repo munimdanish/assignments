a49b5cc8e3a59d2a9bbf5d76dcf746ea953a77f5
munimdanish <munimdanish@gmail.com>
2025-02-09 17:33:52 +0500

    Add files via upload

--- assignments/class-2.js
+++ assignments/class-2.js
@@ -1,4 +1,4 @@
-let a = 10;
+/*let a = 10;
 let b = 15;
 //--------------
 let sum = a + b;
@@ -14,7 +14,29 @@ let div = a / b ;
 console.log(div);
 //---------------
 let mod = a % b ; 
-console.log(mod);
-
-
-                
+console.log(mod);*/
+let budget = Number(prompt("Enter your budget"));
+console.log("Budget:", budget);
+let day = prompt("Enter your day");
+console.log("Day:", day);
+if (day === "Monday" && budget <= 500) {
+ console.log("You choose Deal 1");               
+}
+else if (day === "Monday" && budget > 500 && budget<1000) {
+    console.log("You choose Deal 2");               
+   }
+   else if (day === "Monday" && budget >= 1001) {
+    console.log("You choose Deal 1 + Deal 2");               
+   }
+   else if (day === "Friday" && budget==1000) {
+    console.log("You choose Buffet");               
+   }
+   else if (day === "Sunday" && budget>=500) {
+    let desseert = prompt("Do you Want Dessert ? (Yes/No)");
+   if (desseert=="Yes"){
+       console.log("You choose Deal + Dessert");
+   }
+   else{
+       console.log("You choose Deal only")
+   }
+}
