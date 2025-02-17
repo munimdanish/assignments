1dbbf80e0ca09f9786e15967a350ede3835e8838
munimdanish <munimdanish@gmail.com>
2025-02-17 12:08:36 +0500

    Add files via upload

---
+++ assignments/md.js
@@ -0,0 +1,26 @@
+// let product={
+//     name:"HP Ellite Book",
+//     instock:true,
+//     price:1000,
+//     totalunits:7,
+//     colors:["black","white","grey"],
+// }
+// product["price"] = 100;
+// product.ram = 128;
+// delete product.totalunits;
+// console.log(product)
+// //console.log(product.colors[2])
+// //console.log(product["colors"]["2"])
+// // let resturent = {
+// //     name:"Ichrin Ramen",
+// //     address:`${Math.floor(Math.random()*100)+1}Johnson.Ave,`
+// //     city:
+// // }
+let student={
+    name:"Haider",
+    age:12,
+    subject:["chemistry","physics","Math"],
+    itsenrolled:true,
+
+
+}
