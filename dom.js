d49630e20b2ba412196793820be314143bcf1922
munimdanish <munimdanish@gmail.com>
2025-04-29 07:54:36 +0500

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,20 @@
+// let heading = document.querySelector(".myClass");
+//let heading = document.querySelector("#head")
+//let heading = document.getElementsByTagName("h1")[0]
+//let heading = document.getElementsByClassName("my Class")[0]
+// let heading = document.getElementById("head")
+// heading.innerText = "this heading";
+// heading.style.backgroundColor = "magenta"
+// heading.style.display = "flex"
+// heading.style.justifyContent = "center"
+
+
+// let para = document.createElement("p")
+// let text = document.createTextNode("hello students")
+// para.append(text)
+// let section = document.querySelector("#content-section")
+// section.append(para)
+// section.style.color = "green"
+// section.style.fontSize = "100px"
+
+let heading = document.querySelector("h2").append("from apna college students")
